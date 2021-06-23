0a74131b270ae4bb653b6299ca2087529dc29c80
luna yu <luna.yu@lumaverse.com>
2021-06-23 11:37:17 -0700

    test optinmonster

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import client from '../client'
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return client.fetch('*[_id == "global-config"] {lang}.lang[0]').then(lang => {
-      return {...initialProps, lang}
+      return { ...initialProps, lang }
     })
   }
 
-  render () {
+  render() {
     return (
       <Html lang={this.props.lang || 'en'}>
         <Head />
         <body>
+          <div id="om-itpt2mebuy3izdzynnb4-holder"></div>
           <Main />
           <NextScript />
+          <script type="text/javascript" src="https://a.omappapi.com/app/js/api.min.js" data-account="139257" data-user="126907" async></script>
         </body>
       </Html>
     )
